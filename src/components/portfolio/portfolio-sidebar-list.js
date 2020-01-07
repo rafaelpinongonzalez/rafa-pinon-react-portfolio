ed24134b870ebb02de28a141e93521c87dc3773a
rafael pinon gonzalez <rafapinongonzalez@live.com.mx>
2020-01-06 17:00:53 -0700

    06/01-20 pagina

--- src/components/portfolio/portfolio-sidebar-list.js
+++ src/components/portfolio/portfolio-sidebar-list.js
@@ -1,19 +1,38 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const PortfolioSidebarList = props => {
   const portfolioList = props.data.map(portfolioItem => {
     return (
-      <div>
-        <div>
+      <div key={portfolioItem.id} className="portfolio-item-thumb">
+        <div className="portfolio-thumb-img">
           <img src={portfolioItem.thumb_image_url} />
         </div>
-        <h1>{portfolioItem.name}</h1>
-        <h2>{portfolioItem.id}</h2>
+
+        <div className="text-content">
+          <div className="title">{portfolioItem.name}</div>
+
+          <div className="actions">
+            <a
+              className="action-icon"
+              onClick={() => props.handleEditClick(portfolioItem)}
+            >
+              <FontAwesomeIcon icon="edit" />
+            </a>
+
+            <a
+              className="action-icon"
+              onClick={() => props.handleDeleteClick(portfolioItem)}
+            >
+              <FontAwesomeIcon icon="trash" />
+            </a>
+          </div>
+        </div>
       </div>
     );
   });
 
-  return <div>{portfolioList}</div>;
+  return <div className="portfolio-sidebar-list-wrapper">{portfolioList}</div>;
 };
 
 export default PortfolioSidebarList;
